57a27b8d85293e049c839d90e2d595e0cf734752
Ruslan <ruslan.dzikanchuk@gmail.com>
2025-10-20 19:10:24 +0300

    commited

---
+++ piprfile.py
@@ -0,0 +1,22 @@
+question =[ {
+        'question':'Who is pidoras',
+        "options":['Nikita','Ilya','Ruslan'],
+        'answer':'3'
+    },
+    {
+        'question':'Who is loser',
+        "options":['Nikita','Ilya','Ruslan'],
+        'answer':'3'
+    }
+]
+for i in range(0, len(question)):
+    print(question[i]['question'])
+    for q in range(0, len(question[i]["options"])):
+        print(q+1 , question[i]["options"][q])
+    answer = input()
+    if answer == question[i]['answer']:
+        print('right')
+        score += 1    
+    else:
+        print('Incorrect')
+print('your score is', score)
